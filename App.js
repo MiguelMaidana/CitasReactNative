e23cf94f915ffce5450d9a0a60a5b66ddaf797d3
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-01 12:50:13 -0300

    Creacion de Citas, listado y agreagamos un boton eliminar

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Cita from "./componentes/Citas"
 
 const App =() => {
+
+  const [citas, setCitas] = useState([
+    {id:"1", paciente:"Hook", propietario :"Juan", sintomas:"no duerme"},
+    {id:"2", paciente:"Redux", propietario :"Miguel", sintomas:"no come"},
+    {id:"3", paciente:"Native", propietario :"Tomas", sintomas:"no canta"}
+  ])
   return (
 
       <View style={styles.contenedor}>
-
-        <Text style={styles.titulo}>Administrador de Citas</Text>
-
+        <Text style={styles.titulo}>Administrador de citas</Text>
+        <FlatList
+        data={citas}
+        renderItem ={({item})=>(
+         <Cita item={item} ></Cita>
+        )}
+        keyExtractor={cita =>cita.id}
+        />
 
       </View>
-    
   );
 };
 
