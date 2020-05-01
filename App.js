066e1104bdeede434ed96146be810e92f3e00c91
Miguel Maidana <maidana.miguell@gmail.com>
2020-04-30 23:46:00 -0300

    Comienzo de Proyecto

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App =() => {
+  return (
+
+      <View style={styles.contenedor}>
+
+        <Text style={styles.titulo}>Administrador de Citas</Text>
+
+
+      </View>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+
+  contenedor :{
+    backgroundColor:"#AA076B",
+    flex:1
+  },
+
+  titulo : {
+    color:"#FFF",
+    marginTop : 40,
+    fontSize:24,
+    fontWeight:"bold",
+    textAlign:"center"
+  }
+})
+
+export default App
